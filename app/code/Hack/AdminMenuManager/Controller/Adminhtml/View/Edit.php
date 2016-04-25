1260334edc4f60b5275334cad6364b0e126402ff
Simon Sippert <s.sippert@techdivision.com>
2016-04-25 13:31:11 +0200

    Added view

---
+++ app/code/Hack/AdminMenuManager/Controller/Adminhtml/View/Edit.php
@@ -0,0 +1,15 @@
+<?php
+namespace Hack\AdminMenuManager\Controller\Adminhtml\View;
+
+class Edit extends \Magento\Backend\App\Action
+{
+    /**
+     * Edit Action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        die('foobar');
+    }
+}
