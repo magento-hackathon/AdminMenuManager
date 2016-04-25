c07c02c66c9ff10a2cc26ae2ff1bcc3245fe77b9
Tomáš Marčík <marcikt@magexo.cz>
2016-04-25 13:49:34 +0200

    #5 - examples - reorder, move

--- app/code/Hack/AdminMenuManager/Plugin/MenuBuilder.php
+++ app/code/Hack/AdminMenuManager/Plugin/MenuBuilder.php
@@ -7,6 +7,8 @@ class MenuBuilder {
     public function afterGetResult(\Magento\Backend\Model\Menu\Builder $builder, \Magento\Backend\Model\Menu $menu) {
         $menu->reorder('Magento_Backend::dashboard', 100);
 
+        $menu->move('Magento_Customer::customer_group', 'Magento_Customer::customer', 100);
+
         return $menu;
     }
 }
